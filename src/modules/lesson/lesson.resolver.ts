35ff3d808b4a019f1b66f016025883d578207dd3
Sam Hoeterickx <samhoeterickx111@gmail.com>
2026-04-01 13:58:02 +0200

    feat: added GqlAuthGuard for lesson endpoint

--- src/modules/lesson/lesson.resolver.ts
+++ src/modules/lesson/lesson.resolver.ts
@@ -1,8 +1,11 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { LessonService } from './lesson.service';
 import { Lesson } from './entity/lesson.entity';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/shared/guards/gqlAuth.guard';
 
 @Resolver()
+@UseGuards(GqlAuthGuard)
 export class LessonResolver {
     constructor(private lessonService: LessonService) {}
 
